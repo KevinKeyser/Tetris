28e589cf7de8b28ecbf621a1fbda7860e87e364d
Kevin Keyser <Kevin.Keyser@live.com>
2015-01-08 19:34:51 -0800

    Updated Templates

---
+++ Tetris/Game1.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MiLib.CoreTypes;
+
+namespace Tetris
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Piece straight;
+        Piece l;
+        Piece reverseL;
+        Piece square;
+        Piece z;
+        Piece reverseZ;
+        Piece t;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            IsMouseVisible = true;
+            base.Initialize();
+            graphics.PreferredBackBufferWidth = 1050;
+            graphics.ApplyChanges();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            straight = new Piece(new bool[5][] { new bool[]{ false, false, true, false, false }, new bool[]{ false, false, true, false, false }, new bool[]{ false, false, true, false, false }, new bool[]{ false, false, true, false, false }, new bool[]{ false, false, false, false, false } }, Content.Load<SpriteFont>("font"));
+            l = new Piece(new bool[5][] { new bool[] { false, false, false, false, false }, new bool[] { false, false, true, false, false }, new bool[] { false, false, true, false, false }, new bool[] { false, false, true, true, false }, new bool[] { false, false, false, false, false } }, Content.Load<SpriteFont>("font"));
+            l.Position = new Vector2(150, 0);
+            reverseL = new Piece(new bool[5][] { new bool[] { false, false, false, false, false }, new bool[] { false, false, true, false, false }, new bool[] { false, false, true, false, false }, new bool[] { false, true, true, false, false }, new bool[] { false, false, false, false, false } }, Content.Load<SpriteFont>("font"));
+            reverseL.Position = new Vector2(300, 0);
+            square = new Piece(new bool[5][] { new bool[] { false, false, false, false, false }, new bool[] { false, false, false, false, false }, new bool[] { false, false, true, true, false }, new bool[] { false, false, true, true, false }, new bool[] { false, false, false, false, false } }, Content.Load<SpriteFont>("font"));
+            square.Position = new Vector2(450, 0);
+            square.CanRotate = false;
+            z = new Piece(new bool[5][] { new bool[] { false, false, false, false, false }, new bool[] { false, false, false, false, false }, new bool[] { false, true, true, false, false }, new bool[] { false, false, true, true, false }, new bool[] { false, false, false, false, false } }, Content.Load<SpriteFont>("font"));
+            z.Position = new Vector2(600, 0);
+            reverseZ = new Piece(new bool[5][] { new bool[] { false, false, false, false, false }, new bool[] { false, false, false, false, false }, new bool[] { false, false, true, true, false }, new bool[] { false, true, true, false, false }, new bool[] { false, false, false, false, false } }, Content.Load<SpriteFont>("font"));
+            reverseZ.Position = new Vector2(750, 0);
+            t = new Piece(new bool[5][] { new bool[] { false, false, false, false, false }, new bool[] { false, false, false, false, false }, new bool[] { false, true, true, true, false }, new bool[] { false, false, true, false, false }, new bool[] { false, false, false, false, false } }, Content.Load<SpriteFont>("font"));
+            t.Position = new Vector2(900, 0);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            InputManager.Update();
+
+            if(InputManager.IsKeyPressed(Keys.Left))
+            {
+                straight.RotateLeft();
+                l.RotateLeft();
+                reverseL.RotateLeft();
+                square.RotateLeft();
+                z.RotateLeft();
+                reverseZ.RotateLeft();
+                t.RotateLeft();
+            }
+            if(InputManager.IsKeyPressed(Keys.Right))
+            {
+                straight.RotateRight();
+                l.RotateRight();
+                reverseL.RotateRight();
+                square.RotateRight();
+                z.RotateRight();
+                reverseZ.RotateRight();
+                t.RotateRight();
+            }
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+
+            straight.Draw(spriteBatch);
+            l.Draw(spriteBatch);
+            reverseL.Draw(spriteBatch);
+            square.Draw(spriteBatch);
+            z.Draw(spriteBatch);
+            reverseZ.Draw(spriteBatch);
+            t.Draw(spriteBatch);
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
--- Tetris/Properties/AssemblyInfo.cs
+++ Tetris/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MiLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Tetris")]
+[assembly: AssemblyProduct("Tetris")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MiLib")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("92bdc1c0-ba5f-43c9-87d1-5bc71a3a054d")]
+[assembly: Guid("9bf1ece7-f900-4ab0-a935-b329118b89ed")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.3.0.1809")]
+[assembly: AssemblyFileVersion("3.3.0.1809")]
